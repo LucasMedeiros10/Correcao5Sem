af84cf8166d35b7b50153514200ec69139a25438
Lucas <Lucas@Lucas-PC>
2017-03-20 19:45:29 -0300

    Envio das classes

---
+++ src/br/univel/comum/CONSTANTES.java
@@ -0,0 +1,10 @@
+package br.univel.comum;
+
+public class CONSTANTES {
+	//RA_DEZENA TEM O VALOR DA DEZENA DO SEU RA, OU SEJA
+	//SE TEU RA É 123.456 ENTÃO O RA_DEZENA É 56, OS DOIS 
+	//ÚLTIMOS NÚMEROS. SE O RA É 123.400 ENTÃO O RA_DEZENA 
+	//É 00.
+
+	public static final Integer RA_DEZENA = 26;
+}
